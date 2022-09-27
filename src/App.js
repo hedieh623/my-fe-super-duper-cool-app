b0e5faf7cc4dfb3251ee93c11498f5c85246594a
Hedieh <hedieh6@gmail.com>
2022-09-27 12:29:04 +0100

    task 4 finished

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from "axios"
-import Header from "./Components/Header"
-import Topics from "./Components/Topics"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
 import Articles from "./Components/Articles";
-import Articleid from "./Components/ArticlesID"
-import Comments from "./Components/Comments"
-import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from 'react';
-
-
-
-
 function App() {
-  const [articles, setArticles] = { useState };
   return (
-    <div className="App">
-      <Header />
-      <Articles setArticles={setArticles} />
-      <Routes>
-        <Route path="/" element={<Topics articles={articles} />}></Route>
-        <Route path="/:article_topic" element={<Topics />}></Route>
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+
+        <Articles />
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/Components/Articles.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+const Articles = () => {
+  const [articles, setArticles] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get(`https://my-fe-super-duper-cool-app.herokuapp.com/api/articles`)
+      .then(({ data }) => {
+        console.log(data.articles);
+        setArticles(data.articles);
+      })
+      .catch((err) => {});
+  }, []);
+  return (
+    <main>
+      <ul>
+        {articles.map((article) => {
+          return <li key={article.article_id}>{article.title}</li>;
+        })}
+      </ul>
+    </main>
+    
+
+  );
+
+  
+  
+
+
+
+
+
+
+
+  
+};
+export default Articles;
