c165db9ab6855c3c7c4346e953845a616cee2995
Hedieh <hedieh6@gmail.com>
2022-09-29 11:55:27 +0100

    made an upvote button work

--- src/Components/Votes.jsx
+++ src/Components/Votes.jsx
@@ -3,47 +3,34 @@ import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import SingleArticle from "./SingleArticle";
 
 
-const Votes = ({ setTopicTerm}) => {
-  const [votes, setVotes] = useState("");
+const Votes = (props) => {
+  const [votescount, setVotescount] = useState(0);
+  const[errormsg,seterrormsg]= useState(null)
   const {article_id} = useParams();
-  console.log({ article_id })
-
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://my-fe-super-duper-cool-app.herokuapp.com/api/articles/${article_id}`
-      )
-      .then(({ data }) => {
-        console.log(`data =${JSON.stringify(data)}`);
-        console.log(`data.article =${JSON.stringify(data.article)}`);
-        setVotes(data.article);
-      })
-      .catch((err) => {});
-  }, [article_id]);
+  function handleUpper(){
+    setVotescount((currentVotesCount)=> currentVotesCount+1)
+    axios.patch(
+      `https://my-fe-super-duper-cool-app.herokuapp.com/api/articles/${article_id}`,
+      { inc_votes: 1 }
+    ).catch((err)=>{seterrormsg("Try again later")});
+    ;
+  }
+  if(errormsg){
+    return(<p>{errormsg}</p>)
+  }
   return (
-    <main>
-      <Topics />
-      <ul>
-        {votes.map((vote) => {
-          return (
-            <li key={article.article_id}>
-              <Link to={`/articles/${article.article_id}/`}>
-                <h2>{article.title}</h2>
-              </Link>
-
-              <p>Created At: {article.created_at}</p>
-              <p> Topic: {article.topic}</p>
-              <p>Author: {article.author}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </main>
+    <>
+      <p className="votes"> VOTES: {props.votes + votescount}</p>
+      <button type="Upvote" onClick = {()=>handleUpper()}>Up</button>
+      <button type="Downvote">Down</button>
+    </>
   );
-
   
+
+
 };
+
 export default Votes;
